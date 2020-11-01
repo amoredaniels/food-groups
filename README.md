3a84f54e74f789dbb785a48260d1b8b573a5768e
Amore' Daniels <61096070+amoredaniels@users.noreply.github.com>
2020-11-01 16:21:12 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+# Food Groups (Multidimensional Lists)
+
+The file food_groups.py contains a pair of lists.  One list, groupLabels, contains the labels for the different food groups contained in the second list.  The second list, foodGroups, is a list of food groups.  This is a list of lists.  Each element of foodGroups is a of the foods that belong to the specific group.  
+
+## Instructions: 
+Inspect the contents of the file to better understand its content.  Then, create code to answer the questions below using print statements.  In each case, do not assume to know the index of the different groups within the foodGroups list.  **There should be no hard coded references to items in either list.** Instead, use the groupLabels list to find the different groups as needed. 
+
+**Note:** To make your solutions easier to read print a message identifying the prompt your solution is addressing. For example: "Part A: Solution".
+
+
+**Part A**
+How many different food groups are there?
+
+**Part B**
+How many foods are listed in total?
+
+**Part C**
+What is the index of the "Fruits" group?
+
+**Part D**
+How many items from each food group is listed?  (print type and number)
+example:  There are ## Vegetables, ## Fruits, etc…
+
+**Part E**
+Are there some fruits that are also vegetables?
+
+**Part F**
+List the grains in reverse alphabetical order.
+
+**Part G**
+Create a nested list containing only the first 5 items in each of the original lists.
+
+**Part H**
+Create the potential foundation for a balanced meal by randomly printing one item from each food group.
+
+**Part I**
+Create a single sorted list containing all items in the foodGroups lists.  There should be no repeated items.
+
---
+++ food_groups.py
@@ -0,0 +1,62 @@
+import random
+
+groupLabels = ["Vegetables", "Fruits", "Grains", "Protein", "Dairy"]
+
+foodGroups = [ ["broccoli", "brussels sprouts", "bok choy", "cabbages",
+                "cauliflower", "kale", "lettuce", "silverbeet",
+                "spinach", "snow peas", "potato", "cassava",
+                "sweet potato", "taro", "carrots", "beetroot",
+                "onions", "shallots", "garlic", "bamboo shoots",
+                "swede", "turnip", "red kidney beans", "soybeans",
+                "lima beans", "cannellini beans", "chickpeas",
+                "lentils", "split peas", "tofu", "tomato", "celery",
+                "sprouts", "zucchini", "squash", "avocado", "capsicum",
+                "eggplant", "mushrooms", "cucumber", "okra", "pumpkin",
+                "green peas", "green beans"],
+                ["apples", "avocado", "pears", "oranges", "mandarins",
+                 "grapefruit", "apricots", "cherries", "peaches",
+                 "nectarines", "plums", "bananas", "paw paw", "tomato",
+                 "mangoes", "pineapple", "melons", "berries",
+                 "grapes", "passionfruit"],
+                ["wheat bread", "rye bread", "pita bread", "naan bread",
+                 "focaccia", "crispbreads", "wholegrain cereal", "oats",
+                 "porridge", "muesli", "wholewheat biscuits", "rice",
+                 "barley", "corn", "polenta", "buckwheat", "spelt",
+                 "millet", "sorghum", "triticale", "rye", "quinoa",
+                 "semolina", "pasta", "noodles", "English muffin",
+                 "crumpet", "rice cakes", "couscous", "bulgur",
+                 "popcorn", "flour"],
+                ["beef", "lamb", "veal", "pork", "kangaroo",
+                 "lean sausages", "chicken", "turkey", "duck", "emu",
+                 "goose", "bush birds", "fish", "prawns", "crab",
+                 "lobster", "mussels", "oysters", "scallops", "clams",
+                 "chicken eggs", "duck eggs", "almonds", "pine nuts",
+                 "walnut", "macadamia", "hazelnut", "cashew", "peanut",
+                 "nut spreads", "pumpkin seeds", "sesame seeds",
+                 "sunflower seeds", "brazil nuts", "beans", "lentils",
+                 "chickpeas", "split peas", "tofu"],
+                ["milks", "long life milks", "powdered milk",
+                 "evaporated milk", "soy beverages", "yogurt",
+                 "soy yoghurt", "hard cheeses"] ]
+
+
+#For each problem below, do not assume to know the index of the different groups.
+#Instead, use the groupLabels list to find the different groups as needed.
+
+# Part A How many different food groups are there?
+#print("Part A")
+# Part B How many foods are listed in total?
+
+# Part C What is the index of the "Fruits" group?
+
+# Part D How many items from each food group is listed? (print type and number) example: There are ## Vegetables, ## Fruits, etc…
+
+# Part E Are there some fruits that are also vegetables?
+
+# Part F List the grains in reverse alphabetical order.
+
+# Part G Create a nested list containing only the first 5 items in each of the original lists.
+
+# Part H Create the potential foundation for a balanced meal by randomly printing one item from each food group.
+
+# Part I Create a single sorted list containing all items in the foodGroups lists. There should be no repeated items.
